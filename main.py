0cb6ee0bae2960015d941b555636e70168b07d03
kakarott9595 <119982205+kakarott9595@users.noreply.github.com>
2025-04-29 12:38:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def root():
 @app.get("/upload", response_class=HTMLResponse)
 def upload_form():
     return """
-    <form action="/upload-resume" method="post" enctype="multipart/form-data">
-        <input type="file" name="file"/>
-        <input type="submit"/>
-    </form>
+    <html>
+        <head>
+            <title>Upload Resume</title>
+        </head>
+        <body>
+            <h2>Upload your resume (PDF or DOCX)</h2>
+            <form action="/upload-resume" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" />
+                <input type="submit" value="Upload" />
+            </form>
+        </body>
+    </html>
     """
 
 @app.post("/upload-resume")
