1137db8c3ae356d48d9545c6b688e08a5260ff28
kakarott9595 <119982205+kakarott9595@users.noreply.github.com>
2025-04-29 12:29:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
     return {"message": "Resume Optimizer App is running!"}
+
+@app.get("/upload", response_class=HTMLResponse)
+def upload_form():
+    return """
+    <form action="/upload-resume" method="post" enctype="multipart/form-data">
+        <input type="file" name="file"/>
+        <input type="submit"/>
+    </form>
+    """
+
+@app.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    contents = await file.read()
+    return {"filename": file.filename, "size": len(contents)}
